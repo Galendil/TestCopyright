6f85c6d209be8d0ed3ee3876f7c61ad51b1ef35a
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-12 15:38:19 +0100

    Test copyright

--- .git-add_copyright.sh
+++ .git-add_copyright.sh
@@ -37,7 +37,7 @@ for FILE in $fully_staged_files ; do
     echo "Fixing whitespace and newline in $FILE" >&2
 
     # Replace tabs with four spaces
-    sed -i $'s/\t/    /g' "$FILE"
+    sed -i '' $'s/\t/    /g' "$FILE"
 
     # Strip trailing whitespace
     sed -i '' -E 's/[[:space:]]*$//' "$FILE"
