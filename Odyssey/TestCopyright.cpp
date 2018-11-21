1fce3a2e045845c261f403299d421574bd6b6f49
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-21 19:44:02 +0100

    test

---
+++ Odyssey/TestCopyright.cpp
@@ -0,0 +1,15 @@
+// Copyright 2018 Praxinos, Inc. All Rights Reserved.
+    // Copyright 2018 Praxinos, Inc  . All Rights Reserved.
+
+main()
+{            qsdqsd
+    dqsdqsd
+}dqsd
+qdsihidhqsddqsdqs
+
+            dqsojd dqsdqsdqs\nsuhzd
+dqsdqsd
+
+
+
+qsd
