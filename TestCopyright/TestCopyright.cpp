ae21cc3274c2f9239e87e062824ffb41312d82fb
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-21 18:19:51 +0100

    test

--- TestCopyright/TestCopyright.cpp
+++ TestCopyright/TestCopyright.cpp
@@ -1,10 +1,11 @@
 // Copyright 2018 Praxinos, Inc. All Rights Reserved. 
     // Copyright 2018 Praxinos, Inc  . All Rights Reserved. 
-        	
+    
 main()
-{
+{			
     dqsdqsd
 }
 qdsihidhqsd
-
+    
 dqsojd \nsuhzd
+    
