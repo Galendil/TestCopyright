26c70e2e8bebac0966a70d8753999d6f46921cdb
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-21 18:17:40 +0100

    test

--- TestCopyright/TestCopyright.cpp
+++ TestCopyright/TestCopyright.cpp
@@ -1,12 +1,2 @@
-// Copyright 2018 Praxinos, Inc. All Rights Reserved. 
-                d
-qsdqsd
-// Copyright 2018 Praxinos, Inc. All Rights Reserved. 
-int pied()
-{
-       printdqsd  qsdqsdsf
-       ////////////printf("lul");fsdf
-}
-qds
-		// Copyright 2018 Praxinos, Inc  . All Rights Reserved. 
-	    				    	
+    // Copyright 2018 Praxinos, Inc  . All Rights Reserved. 
+        				    	
