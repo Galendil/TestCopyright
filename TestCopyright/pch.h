28bbc5fc4f3c752d606653860f2077ecb925432b
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-21 14:36:41 +0100

    test

--- TestCopyright/pch.h
+++ TestCopyright/pch.h
@@ -5,7 +5,7 @@
 //   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
 //   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
 //   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
-
+qsdsq
 #ifndef PCH_H
 #define PCH_H
 
