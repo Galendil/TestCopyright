7501dfea2706a35ab90d72767f7be92924b084ea
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-21 17:44:23 +0100

    test

--- TestCopyright/TestCopyright.cpp
+++ TestCopyright/TestCopyright.cpp
@@ -1,5 +1,5 @@
 // Copyright 2018 Praxinos, Inc. All Rights Reserved. 
-
+qs
 
 dqsddqsdqsdqsdsqd
 dqsddsqdqsqdsqdqsdqsd
--- TestCopyright/pch.h
+++ TestCopyright/pch.h
@@ -1,5 +1,5 @@
 // Copyright 2018 Praxinos, Inc. All Rights Reserved. 
-
+qsd
 //   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
 //   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
 //   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
@@ -8,7 +8,7 @@
 //   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
 qsdsqqsdqsd
 #ifndef PCH_Hqsdqsdqsdqsdqsdqsqsdqsd
-#define PCH_H
+#define PCH_H                       
                     
 // TODO: Ajoutez les en-têtes à précompiler ici
 
