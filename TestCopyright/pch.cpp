f3a6708d8ef74538769baca7ddb9e67f01cb8c04
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-21 19:17:37 +0100

    test

--- TestCopyright/pch.cpp
+++ TestCopyright/pch.cpp
@@ -1,7 +1,8 @@
-// Copyright 2018 Praxinos, Inc. All Rights Reserved. 
-        
+// Copyright 2018 Praxinos, Inc. All Rights Reserved.
+
 // pch.cpp : fichier source correspondant à l'en-tête précompilé ; nécessaire au bon déroulement de la compilation
 qdsdsqdqsdqsdqdsqsdqsdqsdqs
 #include "pch.h"
 sqdqsdqsdqsdsdqsd
 // Vous pouvez en général ignorer ce fichier, mais gardez-le si vous utilisez des en-têtes précompilés.
+
