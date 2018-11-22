02ac35a7863379d3ae89f72ee21b717a15d9aef3
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-22 14:04:49 +0100

    test

---
+++ Source/Engine/Odyssey/ANOUS.h
@@ -0,0 +1,14 @@
+// Copyright 2018 Praxinos, Inc. All Rights Reserved.
+                re Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+qsdsqqsdqsd
+#ifndef PCH_Hqsdqssfdqsddqsdqsdqsdqsqsdqsd
+#define PCH_H
+
+// TODO: Ajoutez les en-têtes à précompiler ici
+
+#endif //PCH_H
+
+//Blablablaqsdqsd
---
+++ Source/Engine/Odyssey/dqsd.cpp
@@ -0,0 +1,16 @@
+// Copyright 2018 Praxinos, Inc. All Rights Reserved.
+    // Copyright 2018 Praxinos, Inc  . All Rights Reserved.
+
+main()
+{            qsdqsd
+    dqsdqsdqsd
+}dqsdqsdqsdcqsdsqd
+qdsihidhqsddqsdqsqsdsq
+qsd
+            dqqsqsdqsdsqsdqsdoazeaejqsdqsdd dqsdqsdqsdsqdqsdqsdqs\nsuhzd
+dqsdqsdfsdfsd
+
+qsdqsdqsd
+qsd
+qsdqsdsqd
+sqssqdqsd
