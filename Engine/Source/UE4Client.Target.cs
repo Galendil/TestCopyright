0d1335ed79fd4c9e9c6fd59a0abac7da91b1588c
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-23 16:45:45 +0100

    test

---
+++ Engine/Source/UE4Client.Target.cs
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+//PRAXINOS: CHANGED BEGIN
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using other thing
+using ortifuqhsd*
+using usqudihquidhq
+
+public class UE4ClientTarget : TargetRules
+{
+    public UE4ClientTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Client;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
+		ExtraModuleNames.Add("UE4Game");
+	}
+}
+//PRAXINOS: CHANGED END
