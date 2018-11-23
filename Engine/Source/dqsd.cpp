4c6d4f140a33d6eebd99bc6d5a4b61c535d70f7e
Thomas-Praxinos <thomas.schmitt@praxinos.coop>
2018-11-23 16:17:26 +0100

    test

--- Engine/Source/dqsd.cpp
+++ Engine/Source/dqsd.cpp
@@ -1,34 +1,12 @@
-sedfsdfsdfsdf
-sdf
-sb_typedf
-sb_typedf
-sdfsd
-fsd
-sedfsdfsdfsdf
-sdf
-sbqsdqsd_typedf
-sb_tqsdqsdypedf
-sdfsd
-fsdsedfsdfsdfsdf
-sdf
-sb_typedf
-sb_typqsdqsdsqdedf
-Line changed
+//PRAXINOS: CHANGED BEGIN
+sedfsdqsdqsdqsdfsdfsdf
+//PRAXINOS: CHANGED END
 //PRAXINOS: CHANGED BEGIN
 sdf
-sb_typedqsdqsdf
-sb_typedf
-sdfsd
-fsdsedfsdfsdqsdqsddfsqsdqsdqsddfsedfsdfsdfsdf
-sdf
-This one too
 //PRAXINOS: CHANGED END
+//PRAXINOS: CHANGED BEGIN
 sdfsd
-fsdsedfsdfsdfsdf
+//PRAXINOS: CHANGED END
+sdfsdqqqsdqsdssdqsdsdqsdqqsdqsd
 sdf
-sb_typedf
-sb_typedf
-sdfsdqqsdqsdsdqsdqqsdqsd
-fsd
-
-
+sedfsdqsdqsdqsdfsdfsdf
